38326d72bb9e2ae794bcb2152f83e17d5266cf2c
Slackjaw1431 <55801287+Slackjaw1431@users.noreply.github.com>
2023-04-02 05:41:46 -0400

    Upd

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="mt-5 container">
+      <div className="card">
+        <div className="card-header">
+          Your Todos
+          </div>
+          <div className="card-body">
+            <table className="table table-hover">
+              <thead>
+                <tr>
+                  <th scope='col'>#</th>
+                  <th scope="col">Description</th>
+                  <th scope="col">Owner</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th scope="row">1</th>
+                  <td>Feed Dog</td>
+                  <td>Eric</td>
+                </tr>
+                <tr>
+                  <th scope="row">2</th>
+                  <td>Feed Cat</td>
+                  <td>Eric</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
   );
 }
 
-export default App;
+        export default App;
