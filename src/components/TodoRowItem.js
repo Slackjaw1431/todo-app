4304107a2dc08e99c83616ea5f4028f86d0ad366
Slackjaw1431 <55801287+Slackjaw1431@users.noreply.github.com>
2023-04-02 19:55:25 -0400

    practice items and iteratables

---
+++ src/components/TodoRowItem.js
@@ -0,0 +1,38 @@
+function TodoRowItem() {
+    //must return only one parent
+
+    // const rowNumber = 1;
+    // const rowDescription = "Feed Dog";
+    // const rowOwner = "Eric";
+
+    const item = {
+        rowNumber: 1,
+        rowDescription: "Feed Dog",
+        rowOwner: "Eric",
+    };
+
+    const item2 = {
+        rowNumber: 2,
+        rowDescription: "Feed Cat",
+        rowOwner: "Eric",
+    };
+
+    const todos = [ item, item2 ];
+
+    for (let item of todos) {
+        return (
+          // <tr>
+          //   <th scope="row">{rowNumber}</th>
+          //   <td>{rowDescription}</td>
+          //   <td>{rowOwner}</td>
+          // </tr>
+          <tr>
+            <th scope="row">{item.rowNumber}</th>
+            <td>{item.rowDescription}</td>
+            <td>{item.rowOwner}</td>
+          </tr>
+        );
+    }
+};
+
+    export default TodoRowItem;
