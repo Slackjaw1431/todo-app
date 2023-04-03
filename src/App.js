eeeedbb0084b7e9dc4e4c0f06012a6d246839938
Slackjaw1431 <55801287+Slackjaw1431@users.noreply.github.com>
2023-04-03 01:03:28 -0400

    Button and more functions

--- src/App.js
+++ src/App.js
@@ -1,24 +1,79 @@
+import React, {useState} from 'react'
 import './App.css';
 import TodoTable from './components/TodoTable';
 
-const todosInFile = [
-  {rowNumber : 3, rowDescription: "Feed yourself", rowOwner: "Eric"},
-  {rowNumber : 4, rowDescription: "Feed the world", rowOwner: "Eric"}
-]
+// const todosInFile = [
+//   {rowNumber : 3, rowDescription: "Feed yourself", rowOwner: "Eric"},
+//   {rowNumber : 4, rowDescription: "Feed the world", rowOwner: "Eric"}
+// ]
+
 
 function App() {
+  
+  const [todosInFile, setTodos] = useState([
+    { rowNumber: 3, rowDescription: "Feed yourself", rowOwner: "Eric" },
+    { rowNumber: 4, rowDescription: "Feed the world", rowOwner: "Eric" },
+  ]);
+
+  let counter = todosInFile.length+2;
+
+  // const addTodo = () => {
+  //   if (todosInFile.length > 0) {
+  //     const newTodo = {
+  //       rowNumber: ++counter,
+  //       rowDescription: "Feed the universe",
+  //       rowOwner: "Eric",
+  //     };
+  //     // todosInFile.push(newTodo);
+  //     setTodos(todosInFile => [...todosInFile, newTodo])
+  //     console.log(todosInFile);
+  //   }
+  // };
+
+  function addTodo() {
+    if(counter <= 4){
+      addTodoSmall();
+    }
+    else if (counter >= 5 && counter < 6){
+      addTodoBig();
+    }
+
+  }
+
+  function addTodoBig() {
+    let newTodo = {
+      rowNumber: ++counter,
+      rowDescription: "Feed Galactus",
+      rowOwner: "Eric",
+    };
+    // todosInFile.push(newTodo);
+    setTodos((todosInFile) => [...todosInFile, newTodo]);
+    console.log(todosInFile);
+
+  }
+
+  function addTodoSmall() {
+    let newTodo = {
+      rowNumber: ++counter,
+      rowDescription: "Feed the universe",
+      rowOwner: "Eric",
+    };
+    // todosInFile.push(newTodo);
+    setTodos((todosInFile) => [...todosInFile, newTodo]);
+    console.log(todosInFile);
+
+    }
+
   return (
+
     <div className="mt-5 container">
       <div className="card">
         <div className="card-header">Your Todos</div>
         <div className="card-body">
         <TodoTable 
-        
-        
-        todosInFile = {todosInFile}
-        
-        
+          todosInFile = {todosInFile}
         />
+        <button className='btn btn-primary' onClick={addTodo}>Add new Todo</button>
         </div>
       </div>
     </div>
